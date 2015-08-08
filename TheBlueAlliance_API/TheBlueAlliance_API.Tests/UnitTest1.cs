20d744630d1562ecdbb6b98c624a4d41f4bea0cd
xNovax <aaron@xnovax.net>
2015-08-07 23:21:51 -0400

    Added basic projects

---
+++ TheBlueAlliance_API/TheBlueAlliance_API.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheBlueAlliance_API.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
