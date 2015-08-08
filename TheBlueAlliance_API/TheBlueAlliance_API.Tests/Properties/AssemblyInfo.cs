cb3b292d6f1d79e52860713d6ca9510c3b3277c3
xNovax <aaron@xnovax.net>
2015-08-07 23:24:33 -0400

    Changed assembly information

--- TheBlueAlliance_API/TheBlueAlliance_API.Tests/Properties/AssemblyInfo.cs
+++ TheBlueAlliance_API/TheBlueAlliance_API.Tests/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TheBlueAlliance_API.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tests for TheBlueAlliance_API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("xNovax")]
 [assembly: AssemblyProduct("TheBlueAlliance_API.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
--- TheBlueAlliance_API/TheBlueAlliance_API/Properties/AssemblyInfo.cs
+++ TheBlueAlliance_API/TheBlueAlliance_API/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TheBlueAlliance_API")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A C# library used to access TheBlueAlliance.com data api")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("xNovax")]
 [assembly: AssemblyProduct("TheBlueAlliance_API")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
